7a087c6da3abfa216c188bdd8bfe9f895d41d2fa
Juan Enrique Rojas Sanchez <jrojsanc@everis.com>
2020-08-11 20:58:14 -0500

    Fix demo

--- spring-alibaba/spring-alibaba-oss/src/main/java/com/spring/java/alibaba/oss/util/DemoOssConstants.java
+++
@@ -1,8 +0,0 @@
-package com.spring.java.alibaba.oss.util;
-
-public class DemoOssConstants {
-
-   public static final String BUCKET_NAME = "demo-spring-alibaba-oss"; // Nombre del bucket
-   public static final String BUCKET_LOCATION = "oss-us-west-1"; // Región del bucket
-
-}
