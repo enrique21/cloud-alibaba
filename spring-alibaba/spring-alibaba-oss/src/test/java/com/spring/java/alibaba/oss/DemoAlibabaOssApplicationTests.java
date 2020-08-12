000d8e0138c3795d45b09ffc15764fe4c83880ca
Juan Enrique Rojas Sanchez <jrojsanc@everis.com>
2020-08-11 20:56:16 -0500

    Subiendo demo alibaba cloud oss - spring

---
+++ spring-alibaba/spring-alibaba-oss/src/test/java/com/spring/java/alibaba/oss/DemoAlibabaOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.java.alibaba.oss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAlibabaOssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
